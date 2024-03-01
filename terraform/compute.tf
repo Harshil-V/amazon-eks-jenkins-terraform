3b002ee6d0d4c5a01710ee29fb81d2b54ec3ffc0
Harshil Vyas <54691846+Harshil-V@users.noreply.github.com>
2024-03-01 10:20:22 -0800

    Update compute.tf

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -1,32 +1,25 @@
-
 data "aws_ami" "amazon-linux-2" {
   most_recent = true
   owners = ["amazon"]
   filter {
-    name = "name"
-    values = [
-      "amzn2-ami-hvm-*-x86_64-gp2",
-    ]
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
   filter {
-    name = "owner-alias"
-    values = [
-      "amazon",
-    ]
+    name   = "owner-alias"
+    values = ["amazon"]
   }
 }
 
 resource "aws_instance" "jenkins-instance" {
-  ami             = "${data.aws_ami.amazon-linux-2.id}"
-  instance_type   = "t2.medium"
-  key_name        = "${var.keyname}"
-  #vpc_id          = "${aws_vpc.development-vpc.id}"
-  vpc_security_group_ids = ["${aws_security_group.sg_allow_ssh_jenkins.id}"]
-  subnet_id          = "${aws_subnet.public-subnet-1.id}"
-  #name            = "${var.name}"
-  user_data = "${file("install_jenkins.sh")}"
-
+  ami                         = data.aws_ami.amazon-linux-2.id
+  instance_type               = "t2.medium"
+  key_name                    = var.keyname
+  vpc_security_group_ids      = [aws_security_group.sg_allow_ssh_jenkins.id]
+  subnet_id                   = aws_subnet.public-subnet-1.id
+  user_data                   = file("install_jenkins.sh")
   associate_public_ip_address = true
+
   tags = {
     Name = "Jenkins-Instance"
   }
@@ -35,7 +28,7 @@ resource "aws_instance" "jenkins-instance" {
 resource "aws_security_group" "sg_allow_ssh_jenkins" {
   name        = "allow_ssh_jenkins"
   description = "Allow SSH and Jenkins inbound traffic"
-  vpc_id      = "${aws_vpc.development-vpc.id}"
+  vpc_id      = aws_vpc.development-vpc.id
 
   ingress {
     from_port   = 22
@@ -52,13 +45,13 @@ resource "aws_security_group" "sg_allow_ssh_jenkins" {
   }
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 output "jenkins_ip_address" {
-  value = "${aws_instance.jenkins-instance.public_dns}"
+  value = aws_instance.jenkins-instance.public_dns
 }
