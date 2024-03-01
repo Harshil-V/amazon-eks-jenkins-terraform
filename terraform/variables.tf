c456f6261d00fd3c3dd93d307f184da284388e37
Harshil Vyas <54691846+Harshil-V@users.noreply.github.com>
2024-03-01 10:12:17 -0800

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,36 +1,61 @@
 variable "region" {
-  default = "us-east-1"
+  type        = string
+  default     = "us-east-1"
+  description = "The AWS region where resources will be created."
 }
+
 variable "environment" {
-  default = "Development"
+  type        = string
+  default     = "Development"
+  description = "A name that will be included in the tags of all created resources to identify the environment."
 }
+
 variable "vpc_cidr" {
-  description = "VPC cidr block"
+  type        = string
+  description = "The CIDR block for the VPC."
 }
+
 variable "public_subnet_1_cidr" {
-  description = "Public Subnet 1 cidr block"
+  type        = string
+  description = "CIDR block for the first public subnet."
 }
+
 variable "public_subnet_2_cidr" {
-  description = "Public Subnet 2 cidr block"
+  type        = string
+  description = "CIDR block for the second public subnet."
 }
+
 variable "public_subnet_3_cidr" {
-  description = "Public Subnet 3 cidr block"
+  type        = string
+  description = "CIDR block for the third public subnet."
 }
+
 variable "private_subnet_1_cidr" {
-  description = "Private Subnet 1 cidr block"
+  type        = string
+  description = "CIDR block for the first private subnet."
 }
+
 variable "private_subnet_2_cidr" {
-  description = "Private Subnet 2 cidr block"
+  type        = string
+  description = "CIDR block for the second private subnet."
 }
+
 variable "private_subnet_3_cidr" {
-  description = "Private Subnet 3 cidr block"
+  type        = string
+  description = "CIDR block for the third private subnet."
 }
+
 variable "instance_type" {
-
+  type        = string
+  description = "EC2 instance type for the Jenkins server."
 }
+
 variable "instance_ami" {
-
+  type        = string
+  description = "AMI ID for the EC2 instance."
 }
+
 variable "keyname" {
-  
+  type        = string
+  description = "The name of the SSH key pair to attach to the EC2 instance."
 }
