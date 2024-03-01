c9b0f25857f214ada270146ff39a67e3e7d834cd
Harshil Vyas <54691846+Harshil-V@users.noreply.github.com>
2024-03-01 10:30:33 -0800

    Update install_kubectl_hal.sh

--- terraform/install_kubectl_hal.sh
+++ terraform/install_kubectl_hal.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+# Add the Kubernetes repository
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
 baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
@@ -9,7 +10,16 @@ gpgcheck=1
 repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
-yum install -y kubectl
 
-curl -L https://get.armory.io/halyard/install/latest/macos/InstallArmoryHalyard.sh > InstallArmoryHalyard.sh 
-sudo bash InstallArmoryHalyard.sh --version latest
+# Install kubectl
+sudo yum install -y kubectl
+
+# Install Halyard
+# Note: As of the last update, the official way to install Halyard on Linux is through a Docker container.
+# The following commands install Halyard's Docker container.
+# Ensure Docker is installed and running before executing these commands.
+curl -O https://raw.githubusercontent.com/spinnaker/halyard/master/install/stable/InstallHalyard.sh
+sudo bash InstallHalyard.sh --user $(whoami) --version latest
+
+# Clean up the installer script
+# rm InstallHalyard.sh
